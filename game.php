6321b9e90db952fc16ba7aa8b73f48eb0c605bec
Irina Serysheva 2 <serysheva.i@yandex.ru>
2019-11-29 23:51:20 +0900

    редактирование интерфейса

--- game.php
+++ game.php
@@ -8,40 +8,32 @@
         <title>DOTS</title>
     </head>
     <body>
-        <!-- скрывает содержимое при ajax-запросе -->
-        <div class="filler">    
-        </div>
-        <div class="score-counter">
-            <span class="player1" id="my-name"><?php echo $_GET['username']?><span>
-            <div class="scores">
-                <span class="red-scores" id="red-scores">0</span>/<span class="blue-scores" id="blue-scores">0</span>
-            </div>
-            <span class="player2" id="opponent-name">opponent</span>
-        </div>
-        <div class="winner" id="winner">
-        </div>
-        <!-- <h3 class="header">Welcome to "DOTS"!</h3> -->
         <div class="wrapper">
-            
-            <div class="field-wrapper">
-                <!-- <div class="field"></div> -->
-                <svg data-x="3" data-y="2" class="field" id="field"></svg>
+            <!-- for displaying connection status -->
+            <div class="connection-state" id="connection-state"></div>
+            <div class="score-counter">
+                <!-- !!!!!!!!!!!!!!что с классами!!!!???? -->
+                <div class="player1" id="my-name"><?php echo $_GET['username']?></div>
+                <div class="red-scores" id="red-scores">0</div>
+                <div class="player2" id="opponent-name">opponent</div>
+                <div class="blue-scores" id="blue-scores">0</div>
             </div>
-            <!-- разместить канву ил свг под таблицей -->
-            <div class="tabs-wrapper">
-                <div class="tab">Player</div>
-                <div class="tab">Player</div>
-                <div class="tab">Player</div>
-
+            <!-- <div class="winner" id="winner">
+            </div> -->
+            <div class="field-wrapper">
+                <div class="field"></div>
+                <!-- <svg data-x="3" data-y="2" class="field" id="field"></svg> -->
+            </div>
+            <!-- <div class="tabs-wrapper">
                 <select class = "tab" id="dot_color">
                     <option value="#ff0000">Красный</option>
                     <option value="#0000ff">Синий<option>
                 </select>
-            </div>
+            </div> -->
         </div>
         <!-- скрипты -->
         <script src="scripts\snap.svg.js"></script>
-        <script src="scripts\game.js" type="module"></script>
+        <!-- <script src="scripts\game.js" type="module"></script> -->
         <!-- <script src="scripts\game-gui.js" type="module"></script> -->
     </body>
 </html>
--- scripts/game.js
+++ scripts/game.js
@@ -1,86 +1,105 @@
 // "use strict";
+import {RED_COLOR, BLUE_COLOR} from './classes/game-consts.js'
+
 import {Dot} from './classes/Dot.js';
+// !!!!!!!!!!!!!!!
 import {Surrounding} from './classes/Surrounding.js';
 import {Field} from './classes/Field.js';
 
-var RED_COLOR = '#ff0000', BLUE_COLOR = '#0000ff';
+// for network
+import {GameClient} from './network.js';
+var gameClient = new GameClient(); 
 
-var n = 20;//число клеток
-var field_svg = Snap("#field");//svg элемент содержащий поле
-var w = field_svg.node.clientWidth / n; //ширина клетки
-var h = field_svg.node.clientHeight / n; //высота клетки
+// цветовые константы
+// var RED_COLOR = '#ff0000', BLUE_COLOR = '#0000ff';
+//число клеток
+var n = 20;
+//svg элемент содержащий поле
+var field_svg = Snap("#field");
+//ширина клетки
+var w = field_svg.node.clientWidth / n; 
+//высота клетки
+var h = field_svg.node.clientHeight / n; 
 
 //создание объекта поля
 var gameField= new Field(20, 20);
+// передаем ему его svg
 gameField.svg = field_svg;
 
-// создание сетки
+// обработчик события на точке
+function dotClickHandler(){
+    // меняем цвет на указанный
+    this.node.style.fill = document.getElementById('dot_color').value;
+    //выясняем координаты щелчка
+    var x = +this.node.dataset.x;
+    var y = +this.node.dataset.y;
+
+    if (document.getElementById('dot_color').value==RED_COLOR){
+        gameField.getDot(x,y).setRedColor();
+        gameField.getDot(x,y).setActive(document.getElementById('my-name').innerHTML);
+    }
+    else{
+        gameField.getDot(x,y).setBlueColor();
+        gameField.getDot(x,y).setActive(document.getElementById('opponent-name').innerHTML);
+    }
+
+    // уменьшаем кол-во точек
+    gameField.decFreeDots();
+    // попытка построить полигон
+    if (gameField.trySurround(gameField.getDot(x,y))){
+        // обрабатываем графику
+        var poly = gameField.getLastSurrounding().getPolygonCoords();
+        var polygonSVGcoords = [];
+        for (var i=0;i<poly.length;i++){
+            // копировать несортированные коордиаты
+            var polyX = poly[i].x*w;
+            var polyY = poly[i].y*h;
+            polygonSVGcoords.push(polyX);
+            polygonSVGcoords.push(polyY);
+        }
+        var polygon = field_svg.polygon(polygonSVGcoords);
+        polygon.node.classList = document.getElementById('dot_color').value==RED_COLOR?"red":"blue";
+        // пересчитываем очки
+        document.getElementById('red-scores').innerHTML = gameField.getRedScore();
+        document.getElementById('blue-scores').innerHTML = gameField.getBlueScore();
+    }
+    else{
+        // переключаем игрока(цвет)
+        gameField.toggleColor();
+        document.getElementById('dot_color').querySelector('[value="'+gameField.getCurMoveColor()+'"]').selected = true
+    }
+    // если все поле занято
+    if (gameField.getFreeDots()==0){
+        if (gameField.getRedScore() == gameField.getBlueScore())
+            document.getElementById('winner').innerHTML = 'Ничья!'
+        else if ((gameField.getRedScore() > gameField.getBlueScore()))
+            document.getElementById('winner').innerHTML = 'Красные победили!'
+        else if ((gameField.getRedScore() < gameField.getBlueScore()))
+            document.getElementById('winner').innerHTML = 'Синие победили!'
+    }
+
+}
+
+// создание игрового поля
 for (var i = 0; i<n; i++){
     for (var j = 0; j<n; j++){
         // каждая клетка - прямоугольник
         var cell = field_svg.rect(i*w, j*h, w, h);
+        // присваем клетке ее координты
         cell.node.dataset.x = i;
         cell.node.dataset.y = j;
         // на пересечение клеток(кроме границ) стоят точки(изначально не видны)
         if (i!=0 && j!=0){
+            // каждая точка - круг
             var dot_svg = field_svg.circle(i*w, j*h, 4);
+            // присваем точке ее координаты
             dot_svg.node.dataset.x = i;
             dot_svg.node.dataset.y = j;
-            // обработчик нажатия на svg точки
-            dot_svg.click(function(){
-                // меняем цвет на указанный
-                this.node.style.fill = document.getElementById('dot_color').value;
-                //выясняем координаты щелчка
-                var x = +this.node.dataset.x;
-                var y = +this.node.dataset.y;
-
-                if (document.getElementById('dot_color').value==RED_COLOR){
-                    gameField.getDot(x,y).setRedColor();
-                    gameField.getDot(x,y).setActive(document.getElementById('my-name').innerHTML);
-                }
-                else{
-                    gameField.getDot(x,y).setBlueColor();
-                    gameField.getDot(x,y).setActive(document.getElementById('opponent-name').innerHTML);
-                }
-
-                // уменьшаем кол-во точек
-                gameField.decFreeDots();
-                // попытка построить полигон
-                if (gameField.trySurround(gameField.getDot(x,y))){
-                    // обрабатываем графику
-                    var poly = gameField.getLastSurrounding().getPolygonCoords();
-                    var polygonSVGcoords = [];
-                    for (var i=0;i<poly.length;i++){
-                        // копировать несортированные коордиаты
-                        var polyX = poly[i].x*w;
-                        var polyY = poly[i].y*h;
-                        polygonSVGcoords.push(polyX);
-                        polygonSVGcoords.push(polyY);
-                    }
-                    var polygon = field_svg.polygon(polygonSVGcoords);
-                    polygon.node.classList = document.getElementById('dot_color').value==RED_COLOR?"red":"blue";
-                    // пересчитываем очки
-                    document.getElementById('red-scores').innerHTML = gameField.getRedScore();
-                    document.getElementById('blue-scores').innerHTML = gameField.getBlueScore();
-                }
-                else{
-                    // переключаем игрока(цвет)
-                    gameField.toggleColor();
-                    document.getElementById('dot_color').querySelector('[value="'+gameField.getCurMoveColor()+'"]').selected = true
-                }
-                // если все поле занято
-                if (gameField.getFreeDots()==0){
-                    if (gameField.getRedScore() == gameField.getBlueScore())
-                        document.getElementById('winner').innerHTML = 'Ничья!'
-                    else if ((gameField.getRedScore() > gameField.getBlueScore()))
-                        document.getElementById('winner').innerHTML = 'Красные победили!'
-                    else if ((gameField.getRedScore() < gameField.getBlueScore()))
-                        document.getElementById('winner').innerHTML = 'Синие победили!'
-                }
-
-            });
+            // ставим обработчик нажатия на svg точки
+            dot_svg.click(dotClickHandler);
             // создаем объект точки
             var dot = new Dot(i, j);
+            // передаем ей его svg
             dot.svg = dot_svg;
             // передаем его внутрь поля
             gameField.setDot(i, j, dot);
